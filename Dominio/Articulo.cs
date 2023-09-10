c3f3ef815bda1283312e594cab6f9fc06e27fceb
FanncuS4n <118911524+FanncuS4n@users.noreply.github.com>
2023-09-10 13:44:58 -0300

    Acceso datos, Clase articulo y separación en capas

---
+++ Dominio/Articulo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Articulo
+    {
+        private int codigo;
+        private String nombre;
+        private String descripcion;
+        private String marca;
+        private String categoria;
+        private String imagen;
+        private double precio;
+    }
+}
