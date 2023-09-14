5ab6819ada9bd68cb5ceb856d6455293f7f4969a
FanncuS4n <118911524+FanncuS4n@users.noreply.github.com>
2023-09-14 16:35:35 -0300

    Apuntando a imagenes
    Adición de la clase Imagen y ImagenNegocio.
    Actualización en la clase Articulo, ahora tiene imagen.
    El formulario ahora tiene una PictureBox.

--- Dominio/Articulo.cs
+++ Dominio/Articulo.cs
@@ -18,6 +18,6 @@ namespace dominio
         public Marca Marca { get; set; }
         public Categoria Categoria { get; set; }
         public decimal Precio { get; set; }
-        public string UrlImagen { get; set; }
+        public Imagen Imagen { get; set; }
     }
 }
---
+++ negocio/ImagenNegocio.cs
@@ -0,0 +1,44 @@
+﻿using dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace negocio
+{
+    public class ImagenNegocio
+    {
+        public List<Imagen> listar()
+        {
+            List<Imagen> lista = new List<Imagen>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setConsulta("Select Id, IdArticulo, ImagenUrl From IMAGENES");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Imagen imagen_Actual = new Imagen();
+                    imagen_Actual.ID = (int)datos.Lector["Id"];
+                    imagen_Actual.IdArticulo = (int)datos.Lector["IdArticulo"];
+                    imagen_Actual.ImagenUrl = (string)datos.Lector["ImagenUrl"];
+
+                    lista.Add(imagen_Actual);
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
